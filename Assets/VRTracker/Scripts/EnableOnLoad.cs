5e7b0c4edf48b862167c701fd7e906b816fde2ef
Alex <alex@vrtracker.xyz>
2018-02-13 12:27:54 -0500

    Desactivate other player UI

--- Assets/VRTracker/Scripts/EnableOnLoad.cs
+++ Assets/VRTracker/Scripts/EnableOnLoad.cs
@@ -2,7 +2,8 @@
 using UnityEngine.Networking;
 using UnityEngine;
 
-public class EnableOnLoad : MonoBehaviour {
+public class EnableOnLoad : MonoBehaviour
+{
 
     public bool enableOnLoad = false;
 
@@ -13,33 +14,36 @@ public class EnableOnLoad : MonoBehaviour {
     public Renderer controllerMesh;
 
 
-    protected virtual void Start () {
+    protected virtual void Start()
+    {
 
-		if (transform.parent.GetComponent<NetworkIdentity>() != null && !transform.parent.GetComponent<NetworkIdentity>().isLocalPlayer) {
-			Debug.Log ("Disable Cam");
-			Camera[] cams = GetComponentsInChildren<Camera>();
-			//Desactivate camera of other player
-			foreach(Camera cam in cams)
-			{
-				cam.enabled = false;
-			}
-			//Disable audio listener of other player
-			if(this.GetComponentInChildren<AudioListener>() != null)
-				this.GetComponentInChildren<AudioListener>().enabled = false;
-
-			foreach(Transform child in transform)
-			{
-				//display all the component with tag buddy
-				if (child.tag == "Body") {
-					child.gameObject.layer = 9; //See layer for the number
-					SetLayerToChildren (child.gameObject, 9);
-				}
-					
-			}
-		}
-       else
+        if (transform.parent.GetComponent<NetworkIdentity>() != null && !transform.parent.GetComponent<NetworkIdentity>().isLocalPlayer)
         {
-			Debug.Log ("Enable Cam");
+            Debug.Log("Disable Cam");
+            Camera[] cams = GetComponentsInChildren<Camera>();
+            //Desactivate camera of other player
+            foreach (Camera cam in cams)
+            {
+                cam.enabled = false;
+            }
+            //Disable audio listener of other player
+            if (this.GetComponentInChildren<AudioListener>() != null)
+                this.GetComponentInChildren<AudioListener>().enabled = false;
+
+            foreach (Transform child in transform)
+            {
+                //display all the component with tag buddy
+                if (child.tag == "Body")
+                {
+                    child.gameObject.layer = 9; //See layer for the number
+                    SetLayerToChildren(child.gameObject, 9);
+                }
+
+            }
+        }
+        else
+        {
+            Debug.Log("Enable Cam");
             transform.eulerAngles.Set(transform.eulerAngles.x, 0, transform.eulerAngles.z);
 
             Camera[] cams = GetComponentsInChildren<Camera>();
@@ -52,15 +56,16 @@ public class EnableOnLoad : MonoBehaviour {
             {
                 this.GetComponentInChildren<AudioListener>().enabled = true;
             }
-			foreach(Transform child in transform)
-			{
-				//hide all the component with for the player buddy
-				if (child.tag == "Body") {
-					child.gameObject.layer = 8; //See layer for the number
-					SetLayerToChildren (child.gameObject, 8);
-				}
+            foreach (Transform child in transform)
+            {
+                //hide all the component with for the player buddy
+                if (child.tag == "Body")
+                {
+                    child.gameObject.layer = 8; //See layer for the number
+                    SetLayerToChildren(child.gameObject, 8);
+                }
 
-			}
+            }
         }
 
         //Disable mesh
@@ -78,12 +83,26 @@ public class EnableOnLoad : MonoBehaviour {
         }
     }
 
-	public void SetLayerToChildren(GameObject gObject, int layerNumber)
-	{
-		foreach (Transform trans in gObject.GetComponentsInChildren<Transform>(true))
-		{
-			trans.gameObject.layer = layerNumber;
-		}
-	}
+    public void SetLayerToChildren(GameObject gObject, int layerNumber)
+    {
+        foreach (Transform trans in gObject.GetComponentsInChildren<Transform>(true))
+        {
+            trans.gameObject.layer = layerNumber;
+        }
+    }
+
+    private void DisableUI()
+    {
+        if (transform.parent.GetComponent<NetworkIdentity>() != null && !transform.parent.GetComponent<NetworkIdentity>().isLocalPlayer)
+        {
+            foreach (Transform trans in gameObject.GetComponentsInChildren<Transform>())
+            {
+                if(trans.gameObject.layer == 5)
+                {
+                    trans.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
 
 }
